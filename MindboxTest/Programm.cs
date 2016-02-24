ca3957750f5920b05e3b597635b1e7d0f590dfcd
falcluft <zubko_v@list.ru>
2016-02-24 17:16:50 +0300

    Revert "Revert "Тестовое задание Mindbox""
    This reverts commit 90ce28d8c59d4a641d05095972d27e9357068db2.

---
+++ MindboxTest/Programm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindboxTest {
+	static class Programm {
+		static void Main() {
+			Test();
+			Console.ReadLine();
+		}
+		static void Test() {
+			try {
+				int p = 145;
+				Geometry.RightTriangleSquare(p);
+				Console.WriteLine("Тест провален.");
+			} catch (HandsOutOfAssException ex) {
+				Console.WriteLine(ex.Message);
+				Console.WriteLine("Тест пройден.");
+			}
+		}
+	}
+}
